15ef560a6d30666a8bc1d5f01167a0c559096c41
Gheno <lucasgheno600@gmail.com>
2023-05-05 01:50:08 -0300

    Commit

--- src/br/passagensaereas/fag/Bagagem.java
+++ src/br/passagensaereas/fag/Bagagem.java
@@ -4,5 +4,12 @@ package br.passagensaereas.fag;
 public class Bagagem {
     private Double peso;
     public void setPeso(double peso) {
+        double taxaPorKg = 2.50;
+
+        Bagagem bagagem = new Bagagem();
+
+        bagagem.setPeso(peso);
+
+        double preco = peso * taxaPorKg;
     }
 }
